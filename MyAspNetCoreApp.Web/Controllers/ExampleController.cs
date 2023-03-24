be7ea92de7d5318d2ce14ec1ff30e74b71bdd038
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-24 13:00:34 +0300

    v_04

--- MyAspNetCoreApp.Web/Controllers/ExampleController.cs
+++ MyAspNetCoreApp.Web/Controllers/ExampleController.cs
@@ -4,6 +4,12 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class ExampleController : Controller
     {
+        public class MyViewModel
+        {
+            public string LinkedInUrl { get; set; }
+            public string GithubUrl { get; set; }
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,6 +17,15 @@ namespace MyAspNetCoreApp.Web.Controllers
         public IActionResult NoLayout()
         {
             return View();
+        }
+        public IActionResult Social()
+        {
+            var social = new List<MyViewModel>()
+            {
+                new () {LinkedInUrl="http://www.linkedin.com/in/muhammed-ferit-simsek-119550217"},
+                new () {GithubUrl="http://github.com/cptmfs"}
+            };
+            return View(social);
         }
     }
 }
