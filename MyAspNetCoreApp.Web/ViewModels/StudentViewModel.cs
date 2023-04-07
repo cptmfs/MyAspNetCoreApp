f4bf0dcb92afe1b1790d1c9e22ae0a72a162ebeb
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-04-07 14:41:34 +0300

    v_14

--- MyAspNetCoreApp.Web/ViewModels/StudentViewModel.cs
+++ MyAspNetCoreApp.Web/ViewModels/StudentViewModel.cs
@@ -5,7 +5,12 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Image { get; set; }
+        public string LinkedIn { get; set; }
+        public string GitHub { get; set; }
+        public string Twitter { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
         public string Title { get; set; }
         public string Experiences { get; set; }
         public string Skills { get; set; }
