a986cf55a5590773b9ba18a49348d5d9b7031a44
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-04-15 21:26:38 +0300

    v_Final

---
+++ MyAspNetCoreApp.Web/Controllers/CookieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult CookieCreate()
+        {
+            //Cookie kaydederken context'in response'ından kaydediyoruz
+            HttpContext.Response.Cookies.Append("background-color", "red", new CookieOptions()
+            {
+                Expires=DateTime.Now.AddDays(2) 
+            });
+            return View();
+        }
+        public IActionResult CookieRead()
+        {
+            //Cookie okurken context'in request'inden kaydediryoruz
+
+            var bgcolor = HttpContext.Request.Cookies["background-color"];
+            ViewBag.bgcolor=bgcolor;
+            return View();
+        }
+    }
+}
